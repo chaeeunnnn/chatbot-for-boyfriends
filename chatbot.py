fe547287145dbd3d05323e7a191c5b7d197b1464
22 <95746437+chaeeunnnn@users.noreply.github.com>
2024-04-15 20:50:03 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,13 +4,14 @@ import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[View the source code](https://github.com/chaeeunnnn/chatbot-for-boyfriends/tree/main)"
+    
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("❤️‍🔥 Chatbot, but romantic ")
+st.caption("💬 A chatbot for boyfriends who are struggling with their girls' hypothetical & silly question attacks, powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "system" , "content": "You are a romantic conversation helper. You assist guys generate a stupidly romantic answer to their girl friends' silly and hypothetical question."},
+                                    {"role": "assistant", "content": "🌹 What silly question did your girlfriend ask most recently? I'll suggest an answer for it. (eg. If I were a worm, would you still love me?)"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
